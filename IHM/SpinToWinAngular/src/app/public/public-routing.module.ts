d61aa4bfea5dedb3107b1989acb04003eea6269b
Valentin Lefebvre <lefebvrevalentin0123@gmail.com>
2024-03-28 15:26:22 +0100

    Ajout de commentaires sur le routage de l'IHM

--- IHM/SpinToWinAngular/src/app/public/public-routing.module.ts
+++ IHM/SpinToWinAngular/src/app/public/public-routing.module.ts
@@ -4,14 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayoutComponent } from './playout/playout.component';
 
+// Routage des pages du module Public 
 const routes: Routes = [
   {
-    path: '', component: PlayoutComponent, children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
+    path: '', component: PlayoutComponent, children: [ 
+      { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirige vers la page d'accueil lorsque le chemin est vide
+      { path: 'home', component: HomeComponent }, // Charge le composant HomeComponent lorsque le chemin est '/home'
     ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
