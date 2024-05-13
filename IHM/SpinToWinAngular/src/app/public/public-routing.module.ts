d2dd118d452b737eec492d8a231253b82e04a3ef
VincentChaye <vincent.chaye@icloud.com>
2024-05-13 11:54:02 +0200

    liaison de page acceuil et mise en plce de mon environement

--- IHM/SpinToWinAngular/src/app/public/public-routing.module.ts
+++ IHM/SpinToWinAngular/src/app/public/public-routing.module.ts
@@ -8,19 +8,20 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { CreditComponent } from './credit/credit.component';
 import { SpintowinComponent } from './spintowin/spintowin.component';
+import { ProposComponent } from './propos/propos.component';
 
 // Routage des pages du module Public 
 const routes: Routes = [
   {
     path: '', component: PlayoutComponent, children: [ 
-      { path: '', redirectTo: 'Vegastudio', pathMatch: 'full' }, // Redirige vers la page d'accueil lorsque le chemin est vide
+      { path: '', redirectTo: 'propos', pathMatch: 'full' }, // Redirige vers la page d'accueil lorsque le chemin est vide
       { path: 'Vegastudio', component: VegastudioComponent }, // Charge le composant VegastudioComponent lorsque le chemin est '/Vegastudio'
       { path: 'SpinToWin', component: SpintowinComponent }, // Charge le composant HomeComponent lorsque le chemin est '/home'
       { path: 'roulette', component: RouletteComponent }, // Charge le composant RouletteComponent lorsque le chemin est '/roulette'
       { path: 'register', component: RegisterComponent }, // Charge le composant RegisterComponent lorsque le chemin est '/register'
       { path: 'login', component: LoginComponent }, // Charge le composant LoginComponent lorsque le chemin est '/login'
       { path: 'credit', component: CreditComponent }, // Charge le composant CreditComponent lorsque le chemin est '/credit'
-
+      { path: 'propos', component: ProposComponent },
     ]
   }
 ];
