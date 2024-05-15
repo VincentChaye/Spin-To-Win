1864c0cd382a741577e037ccf75e3b49d9affaec
Valentin Lefebvre <lefebvrevalentin0123@gmail.com>
2024-05-15 11:26:41 +0200

    correction beug public.module

---
+++ API/spintowin/src/test/java/fr/iut/univcotedazur/spintowin/SpintowinApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.univcotedazur.spintowin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpintowinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
