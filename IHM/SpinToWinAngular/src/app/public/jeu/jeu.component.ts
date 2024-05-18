00171b461269b72c35ebfb226a94af254e868537
grey <leminemahjoub@gmail.com>
2024-05-18 04:37:59 +0200

    Modification Routage : Credit Ajout Jeu

---
+++ IHM/SpinToWinAngular/src/app/public/jeu/jeu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jeu',
+  standalone: true,
+  imports: [],
+  templateUrl: './jeu.component.html',
+  styleUrl: './jeu.component.css'
+})
+export class JeuComponent {
+
+}
